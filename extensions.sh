9d37612e4fdfdf4299fef59074f3e9fd73d42481
Artsiom Balakir <abalak8@illinois.edu>
2019-02-10 23:19:10 -0600

    initial configuration

---
+++ extensions.sh
@@ -0,0 +1,8 @@
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension gharveymn.nightswitch-lite
+code --install-extension GrapeCity.gc-excelviewer
+code --install-extension mechatroner.rainbow-csv
+code --install-extension mitaki28.vscode-clang
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension vscodevim.vim
