29e50b7b53758f70be79ab79b0d8ddf48e64d7f7
Artsiom Balakir <abalak8@illinois.edu>
2019-12-02 22:44:32 -0600

    updated profiles

--- extensions.sh
+++ extensions.sh
@@ -1,14 +1,17 @@
 code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension eamodio.gitlens
+code --install-extension freebroccolo.reasonml
 code --install-extension Gimly81.fortran
 code --install-extension GrapeCity.gc-excelviewer
+code --install-extension kriegalex.vscode-cudacpp
 code --install-extension mechatroner.rainbow-csv
 code --install-extension mitaki28.vscode-clang
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension naumovs.color-highlight
 code --install-extension redhat.java
+code --install-extension tomoki1207.pdf
 code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscjava.vscode-java-debug
 code --install-extension vscjava.vscode-java-dependency
